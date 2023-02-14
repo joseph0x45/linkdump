6b6f5c710dda10a1363e2b17ebda31d715553fe4
TheWisePigeon <zozozozeph@gmail.com>
2023-02-14 14:15:57 +0000

    added authentication page

--- extension/src/pages/auth.svelte
+++ extension/src/pages/auth.svelte
@@ -1,1 +1,17 @@
-<h1>Bozo</h1>
+<script lang="ts" >
+    let email = ""
+    let password = ""
+    function authenticate(){
+
+    }
+</script>
+
+<main class="  flex flex-col items-center justify-center h-screen" >
+    <form class="flex flex-col gap-3" on:submit={authenticate} >
+        <input class=" focus:outline-none p-2 rounded-md text-slate-500 border-[2px] w-64 " type="email" placeholder="Your email">
+        <input class=" focus:outline-none p-2 rounded-md text-slate-500 w-64 border-[2px]" type="password" placeholder="Your password">
+        <button type="submit" class=" rounded-md border-white text-white bg-slate-700 p-2" >
+            Authenticate
+        </button>
+    </form>
+</main>
--- extension/src/pages/index.svelte
+++ extension/src/pages/index.svelte
@@ -1,11 +1,9 @@
 <script lang="ts" >
     import { navigate } from "svelte-routing"
-    function breuh(){
-        navigate("/auth")
-    }
-
 </script>
 
-<button class=" bg-red-400 text-white" on:click={breuh} >
-    Hello
-</button>
+<main>
+    <button on:click={()=>{ navigate("/auth") }} >
+        Auth
+    </button>
+</main>
