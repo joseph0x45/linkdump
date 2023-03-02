acc4fbb1b45cd4557ce98607df78ab6b6e1ae0d1
TheWisePigeon <zozozozeph@gmail.com>
2023-03-02 20:37:25 +0000

    tests: prisma

--- api/src/server.ts
+++ api/src/server.ts
@@ -5,6 +5,7 @@ dotenv.config()
 const app = express()
 app.use(express.json())
 app.use(cors())
+import prisma from "../prisma"
 
 const PORT = process.env.PORT || 5000
 
@@ -13,6 +14,26 @@ app.get("/online", (_, res: Response)=>{
     return res.status(200).send()
 })
 
+app.get("/test", async (_req: Request,res: Response)=>{
+    try {
+        await prisma.dump.create({
+            data:{
+                name:"ye",
+                noice:10,
+                creator:"",
+                mods:["someone", "anotherone"],
+                banner:"ouep"
+            }
+        })
+        const result = await prisma.dump.findMany()
+        return res.status(200).send({
+            result
+        })
+    } catch (error) {
+        console.log(error)
+        return res.status(500).send({})
+    }
+})
 
 app.listen(PORT, ()=>{
     console.log(`App listening on port ${PORT}`)
