ac14d432505fadbf0df18f4cc84d0b20d5e79acd
TheWisePigeon <zozozozeph@gmail.com>
2023-05-19 22:56:59 +0000

    remove old api

--- api/src/routes/auth/index.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import AuthService from "../../services/Auth";
-
-const router = Router()
-
-router.route("/register").post( AuthService.register )
-router.route("/login").post( AuthService.login )
-
-export default router
--- api/src/types.d.ts
+++
@@ -1,1 +0,0 @@
-export type Log = "Warning" | "Error" 
