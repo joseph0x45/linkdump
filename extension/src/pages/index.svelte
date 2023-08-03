1869ca25f9fbc134cdd03688697f565f66603fbe
TheWisePigeon <zozozozeph@gmail.com>
2023-08-03 20:41:15 +0000

    removing node app

--- extension/src/pages/index.svelte
+++ extension/src/pages/index.svelte
@@ -1,20 +1,5 @@
 <script>
-    import { navigate } from "svelte-routing"
-    import { onMount } from "svelte";
-    import { auth } from "../store";
-    onMount( async ()=>{
-        const user = localStorage.getItem("user") ?? ""
-        if(user===""){
-            auth.set({ state:false, user })
-            return
-        }
-        auth.set({ state:true, user })
-    })
 </script>
 
-<h1 class="text-white" >{ $auth.state?"Logged in":"Nah" }</h1>
-<main class=" h-screen w-full flex flex-col items-center justify-center" >
-    <button class=" btn-v2" on:click={()=>{ navigate("/auth") }} >
-        click me mf
-    </button>
+<main class=" bg-red-500 h-screen w-full flex flex-col items-center justify-center" >
 </main>
